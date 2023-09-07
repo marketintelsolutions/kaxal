97d51b998d679f339c436cf06dc18bdf9f6639af
igee-17 <igbagboleye@gmail.com>
2023-09-07 23:12:32 +0100

    new changes

--- src/utils/helpers/arrangeText.js
+++ src/utils/helpers/arrangeText.js
@@ -1,17 +1,17 @@
 export function limitStringTo50Characters(inputString) {
-    if (inputString.length <= 120) {
+    if (inputString.length <= 110) {
         return inputString;
     } else {
         // Find the last space before the 70th character
-        const lastSpaceIndex = inputString.lastIndexOf(" ", 120);
+        const lastSpaceIndex = inputString.lastIndexOf(" ", 110);
 
         // If no space is found before the 70th character, cut the string at the 70th character
         if (lastSpaceIndex === -1) {
-            return inputString.slice(0, 120);
+            return inputString.slice(0, 110);
         } else {
             return inputString.slice(0, lastSpaceIndex);
         }
 
-        // return inputString.slice(0, 120);
+        // return inputString.slice(0, 110);
     }
 }
