e91cab29c3e485128e4f32c98770b5cc431ce3a6
igee-17 <igbagboleye@gmail.com>
2023-09-14 21:44:21 +0100

    twitter added

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import news from '../assets/images/news.png'
+import { Timeline } from 'react-twitter-widgets';
+import { TwitterTimelineEmbed } from 'react-twitter-embed';
+
 
 const News = () => {
     return (
-        <section>
+        <section className='news'>
             <div className="section-one" style={{ backgroundImage: `url(${news})` }}>
                 <div className="section-center">
                     <h2>News</h2>
@@ -13,6 +16,19 @@ const News = () => {
                         <p> {" > News"}</p>
                     </div>
                 </div>
+            </div>
+            <div className="section-two">
+                <h2>- LATEST -</h2>
+                <Timeline
+                    dataSource={{
+                        sourceType: 'profile',
+                        screenName: 'igee_17' // Replace with the Twitter handle of the profile you want to display
+                    }}
+                    options={{
+                        height: '400', // You can customize the height as needed
+                        width: '100%'
+                    }}
+                />
             </div>
         </section>
     )
