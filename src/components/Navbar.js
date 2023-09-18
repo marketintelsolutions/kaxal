4e034e42eb58abedc7a39c107e53b16737f9f18e
igee-17 <igbagboleye@gmail.com>
2023-09-19 00:35:33 +0100

    responsiveness in progress

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/images/logo.png";
+import hamburger from "../assets/logos/hamburger.png";
 import { paths } from "../utils/data";
 
 const Navbar = () => {
-  const [currentPath, setCurrentPath] = useState('/')
+  const [currentPath, setCurrentPath] = useState('/');
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
-  const location = useLocation()
-
-
+  const location = useLocation();
 
   useEffect(() => {
-    window.scroll(0, 0)
-
-    setCurrentPath(location.pathname)
-
-  }, [location])
+    window.scroll(0, 0);
+    setCurrentPath(location.pathname);
+  }, [location]);
 
   return (
     <nav className="navbar">
@@ -23,16 +21,26 @@ const Navbar = () => {
         <div className="logo">
           <img src={logo} alt="logo" />
         </div>
-        <div className="nav-links">
-          {
-            paths.map((pathItem) => {
-              const { text, path } = pathItem
 
-              return <Link to={path} className={`${currentPath === path ? "link-item active" : 'link-item'}`}>
+        {/* Hamburger Button */}
+        <button className="hamburger" onClick={() => setIsNavOpen(!isNavOpen)}>
+          <img src={hamburger} alt="hamburger" />
+        </button>
+
+        <div className={`nav-links ${isNavOpen ? "open" : ""}`}>
+          {paths.map((pathItem, index) => {
+            const { text, path } = pathItem;
+            return (
+              <Link
+                key={index}
+                to={path}
+                className={`${currentPath === path ? "link-item active" : 'link-item'}`}
+                onClick={() => setIsNavOpen(false)} // Close nav when a link is clicked
+              >
                 {text}
               </Link>
-            })
-          }
+            );
+          })}
         </div>
       </div>
     </nav>
